0dace1715e00912327f64cd2ce6280fbf78416b2
Roger B <myvarequals@gmail.com>
2018-07-11 22:52:44 -0400

    finished first assignement

--- src/App.js
+++ src/App.js
@@ -12,25 +12,52 @@ class App extends Component {
     ]
   }
 
-  switchNameHandler = () => {
+  switchNameHandler = (newName) => {
    // console.log('switched')
    this.setState({
      persons: [
-       {name: 'Rogerrr', age: 27},
+       {name: newName, age: 27},
        {name: 'Leta', age: 27},
        {name: 'Brad', age: 28}
      ]
    })
   }
 
+  nameChangeHandler = (event) => {
+    this.setState({
+      persons: [
+        {name: 'Rog', age: 27},
+        {name: event.target.value, age: 27},
+        {name: 'Brad', age: 28}
+      ]
+    })
+  }
+
   render() {
+
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    }
+
     return (
       <div className="App">
         <h1>React App</h1>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} >Hellooo</Person>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+        <button style={style} onClick={() => this.switchNameHandler('ROGER')}>Switch Name</button>
+        <Person
+         name={this.state.persons[0].name} 
+         age={this.state.persons[0].age} />
+        <Person
+         name={this.state.persons[1].name}
+         age={this.state.persons[1].age}
+         click={this.switchNameHandler.bind(this, 'Rog')} 
+         changed={this.nameChangeHandler}>Hellooo</Person>
+        <Person
+         name={this.state.persons[2].name}
+         age={this.state.persons[2].age} />
       </div>
 
     );
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import './Person.css';
 
 
 const person = props => {
     return (
-        <div>
-            <p>Name: {props.name} Age: {props.age}</p>
+        <div className='Person'>
+            <p onClick={props.click}>Name: {props.name} Age: {props.age}</p>
             <p>{props.children}</p>
+            <input type='text' onChange={props.changed} value={props.name} />
         </div>
     )
 };

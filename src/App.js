bdca7a398202ed39084b4d02ce4a3e9eaa4fd312
Roger B <myvarequals@gmail.com>
2018-07-13 04:00:16 -0400

    moving on to next module

--- src/App.js
+++ src/App.js
@@ -6,21 +6,31 @@ class App extends Component {
 
   state = {
     persons: [
-      {name: 'Roger', age: 27},
-      {name: 'Leta', age: 27},
-      {name: 'Brad', age: 28}
+      { id: 'aaa', name: 'Roger', age: 27},
+      { id: 'bbb', name: 'Leta', age: 27},
+      { id: 'ccc', name: 'Brad', age: 28}
     ],
     showPersons: false,
   }
 
 
-  nameChangeHandler = (event) => {
+  nameChangeHandler = (event, id) => {
+
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id
+    })
+
+    const person = {
+      ...this.state.persons[personIndex]
+    }
+
+   person.name = event.target.value
+
+   const persons = [...this.state.persons]
+   persons[personIndex] = person
+
     this.setState({
-      persons: [
-        {name: 'Rog', age: 27},
-        {name: event.target.value, age: 27},
-        {name: 'Brad', age: 28}
-      ]
+      persons: persons
     })
   }
 
@@ -54,7 +64,10 @@ class App extends Component {
             return <Person
                 click={() => this.deletePersonHandler(index)}
                 name={person.name}
-                age={person.age} />
+                age={person.age}
+                key={person.id}
+                changed={(event) => this.nameChangeHandler(event, person.id)} />
+                
           })}
         </div> 
       )
